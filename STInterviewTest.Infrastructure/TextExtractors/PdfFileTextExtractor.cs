832b154d01c1eba8abe7ea143bc1b800c4e0f60b
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2015-02-02 04:03:10 +0200

    Added default extractors for pdf and txt files

---
+++ STInterviewTest.Infrastructure/TextExtractors/PdfFileTextExtractor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using NLog;
+
+namespace STInterviewTest.Infrastructure.TextExtractors
+{
+    public sealed class PdfFileTextExtractor : BaseTextExtractor
+    {
+        private readonly static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #region ITextExtractor Members
+
+        /// <summary>
+        /// Extracts the text of a PDF file using a SimpleTextExtractionStrategy object and iTextSharp
+        /// </summary>
+        /// <param name="filePath">The file path of the file</param>
+        /// <returns>The text content of the file</returns>
+        public override string ExtractText(string filePath)
+        {
+            var fileType = filePath.GetFileType();
+
+            if (fileType != FileTypes.PdfFile)
+            {
+                Logger.Error("File [{0}] cannot be processed by a PdfFileTextExtractor", filePath);
+                throw new NotSupportedException("File [" + filePath + "] cannot be processed by a PdfFileTextExtractor");
+            }
+
+            var result = string.Empty;
+
+            try
+            {
+                var pdfReader = new PdfReader(filePath); 
+                var writer = new StringWriter();
+                var noOfPages = pdfReader.NumberOfPages;
+
+                for (var pageIndex = 1; pageIndex <= noOfPages; pageIndex++)
+                    writer.WriteLine(PdfTextExtractor.GetTextFromPage(pdfReader, pageIndex, new SimpleTextExtractionStrategy()));
+
+                result = writer.ToString().RemoveNewLineAndLineFeed();
+            }
+            catch (Exception exception)
+            {
+                Logger.ErrorException("Cannot parse file", exception);
+            }
+
+            return result;
+        }
+
+        #endregion ITextExtractor Members
+    }
+}
---
+++ STInterviewTest.Infrastructure/TextExtractors/TxtFileTextExtractor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using NLog;
+
+namespace STInterviewTest.Infrastructure.TextExtractors
+{
+    public class TxtFileTextExtractor : BaseTextExtractor
+    {
+        private readonly static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #region ITextExtractor Members
+
+        /// <summary>
+        /// Extracts the text content of a txt file.
+        /// </summary>
+        /// <param name="filePath">The file path of the file</param>
+        /// <returns>The text content of the file</returns>
+        public override string ExtractText(string filePath)
+        {
+            var fileType = filePath.GetFileType();
+
+            if (fileType != FileTypes.TxtFile)
+            {
+                Logger.Error("File [{0}] cannot be processed by a TxtFileTextExtractor", filePath);
+                throw new NotSupportedException("File [" + filePath + "] cannot be processed by a TxtFileTextExtractor");
+            }
+
+            return File.ReadAllText(filePath).RemoveNewLineAndLineFeed();;
+        }
+
+        #endregion ITextExtractor Members
+    }
+}
