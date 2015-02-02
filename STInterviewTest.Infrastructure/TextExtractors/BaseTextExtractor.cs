93dd4a36fe2ba63643ee078ccf579c21c212e97b
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2015-02-02 04:00:40 +0200

    Added definitions for text extractors.

---
+++ STInterviewTest.Infrastructure/TextExtractors/BaseTextExtractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace STInterviewTest.Infrastructure.TextExtractors
+{
+    public abstract class BaseTextExtractor : ITextExtractor, IProcessPipelineTask
+    {
+        private readonly static Logger Logger = LogManager.GetCurrentClassLogger();
+
+        #region IPipelineTask Members
+
+        /// <summary>
+        /// Extracts the text of a txt or pdf file.
+        /// The method takes and returns the same object (with additional data) for easy method chaining.
+        /// </summary>
+        /// <param name="task">The pipeline task witht the input data</param>
+        /// <returns>The pipeline task with the input and the output data</returns>
+        public PipelineTask DoWork(PipelineTask task)
+        {
+            if (task == null)
+            {
+                Logger.Error("Null task received");
+                throw new ArgumentNullException("task");
+            }
+
+            task.ExtractedText = this.ExtractText(task.ActualFilePath);
+
+            Logger.Info("BaseTextExtractor.DoWork finshed");
+
+            return task;
+        }
+
+        #endregion IPipelineTask Members
+
+        public abstract string ExtractText(string filePath);
+    }
+}
---
+++ STInterviewTest.Infrastructure/TextExtractors/ITextExtractor.cs
@@ -0,0 +1,13 @@
+﻿
+namespace STInterviewTest.Infrastructure.TextExtractors
+{
+    public interface ITextExtractor
+    {
+        /// <summary>
+        /// Extracts the text of a given file
+        /// </summary>
+        /// <param name="filePath">The file path of the file</param>
+        /// <returns>The text content of the file</returns>
+        string ExtractText(string filePath);
+    }
+}
